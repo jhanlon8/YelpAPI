4def6e33da3e04418bf797e7fb94a102e08752f4
James Hanlon <jaysonhanlon8@gmail.com>
2021-02-24 19:36:26 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+import json
+from pprint import pprint
+
+myCity = input("What is your city? ")
+FoodChoice = input("What would you like to search for? ")
+
+API_key = 'ZS_xVpKyrEDZazzr_RUo94a1ZLiibK-s1r_A-4nKSV51D6fNEiudwBipk6OWHGUMPpjS3xZIc705ZZV70ngber97a0Iovwg7XKFNgk8Q1NbKamuk9Ule6moVo58cYHYx'
+client_id = 'F1oAG49WJt-yVXvdyKnU7w'
+ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
+HEADERS = {'Authorization': 'Bearer %s' % API_key}
+
+PARAMETERS = {
+	'term' : FoodChoice,
+	'limit' : 10,
+	'location' : myCity,
+	'radius' : 10000
+}
+
+response = requests.get(url = ENDPOINT, params = PARAMETERS, headers = HEADERS)
+data = response.json()
+
+
+for place in data['businesses']:
+	print(place['name'])
+	print(place['location']['display_address'])
